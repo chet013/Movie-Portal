a29adf3ab9978cfc79868d26af5e2da2d4696635
MC_SLAVA <devuser03@devuser03.devuser03>
2025-01-21 16:37:52 +0400

    move page

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -1,38 +1,47 @@
 import styles from './index.module.css';
+import { useNavigate } from 'react-router-dom';
 import { Loader } from '../../Components/loader';
 import { MovieCard } from '../../Components/movie-card';
-
 
 import React from 'react';
 import { useGetMoviesQuery } from '../../features/movieApiSlice';
 
-
 export const Home = () => {
+    const navigate = useNavigate();
     const { data, error, isLoading } = useGetMoviesQuery();
 
-    if (error) return <p className={styles.home}>Failed to load movies. Try again later.</p>;
+    if (error) {
+        return <p className={styles.home}>Failed to load movies. Try again later.</p>;
+    }
+
+    const handleNavigate = (movieId) => {
+        navigate(`/movie/${movieId}`);
+        console.log(data)
+    };
 
     return (
-
         <div className={styles.home}>
-            {
-                isLoading ?
-                    <Loader /> :
-                    <div className={styles.movieList}>
-                        <h1>Movies</h1>
-                        {
-                            data?.Search.map((movie) => (
-                                <MovieCard
-                                    className={styles.movieItem}
-                                    key={movie.imdbID}
-                                    poster={movie.Poster}
-                                    title={movie.Title}
-                                    year={movie.Year}
-                                />
-                            ))
-                        }
-                    </div>
-            }
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div className={styles.movieList}>
+                    <h1>Movies</h1>
+                    {data?.Search.map((movie) => (
+                        <button
+                            key={movie.imdbID}
+                            className={styles.movieButton}
+                            onClick={() => handleNavigate(movie.imdbID)}
+                        >
+                            <MovieCard
+                                className={styles.movieItem}
+                                poster={movie.Poster}
+                                title={movie.Title}
+                                year={movie.Year}
+                            />
+                        </button>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
